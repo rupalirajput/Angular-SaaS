02fd6692cdb8f82613dc96a128ff11597ebfccf5
Jake Ladera <laderah@seattleu.edu>
2019-05-28 16:54:53 -0700

    Adding take-test for the first question in a test

--- src/app/services/take-test-api.service.ts
+++ src/app/services/take-test-api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import ITakeTestModel from '../share/ITakeTestModel';
+import IQuestionModel from '../share/IQuestionModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TakeTestApiService {
 
-  constructor() { }
+  hostUrl = 'http://localhost:1234/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getFirstQuestion(questionBankID: Number)
+  {
+    return this.httpClient.get<IQuestionModel[]>(this.hostUrl + 'test/' + questionBankID )
+  }
 }
--- src/app/take-test/take-test.component.ts
+++ src/app/take-test/take-test.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { TakeTestServiceApi } '../services/take-test-api.service';
+import { TakeTestApiService } from '../services/take-test-api.service';
+import ITakeTestModel from '../share/ITakeTestModel';
 
 @Component({
   selector: 'app-take-test',
@@ -10,8 +11,37 @@ import { TakeTestServiceApi } '../services/take-test-api.service';
   styleUrls: ['./take-test.component.css']
 })
 export class TakeTestComponent implements OnInit {
+  questionBankID: Number;
+  testTakerID: Number;
+  currentQuestionID: Number;
+  orderOfQuestionInTest: Number;
+  questionText: String;
+  category: String;
+  options: [String, String, String, String];
+  answer: String;
+  isCorrect: Number;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private test$: TakeTestApiService
+  ) {
+    this.questionBankID = +route.snapshot.params['id'];
+    test$.getFirstQuestion(this.questionBankID)
+    .subscribe(
+      result => {
+        this.questionBankID = result[0].questionBankID;
+        this.currentQuestionID = result[0].questionID;
+        this.orderOfQuestionInTest = 0;
+        this.questionText = result[0].questionText;
+        this.category = result[0].category;
+        this.options = result[0].options;
+        this.answer = result[0].answer;
+      },
+      () => {},
+      () => {}
+    );
+  }
 
   ngOnInit() {
   }
