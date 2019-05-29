81eabfd0e8cedcb00e88dc38f39879a26c324560
Jake Ladera <laderah@seattleu.edu>
2019-05-29 12:17:47 -0700

    Added back in my take test component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { ReportComponent } from './report/report.component';
 import {QuesBankTableComponent} from './ques-bank-table/ques-bank-table.component';
-
 import {QuestionComponent} from './question/question.component';
 import { TestDetailsComponent } from './test-details/test-details.component';
+import { TakeTestComponent } from './take-test/take-test.component';
+
 const routes: Routes = [
   { path: 'account', component: AccountComponent },
   { path: 'reports', component: ReportComponent },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'professor_dashboard', component: QuesBankTableComponent },
   { path: 'create_question', component: QuestionComponent },
   { path: 'test/detail/:id', component: TestDetailsComponent},
+  { path: 'test/:questionbankid', component: TakeTestComponent },
   { path: 'ques-bank-table', component: QuesBankTableComponent },
   { path: 'student_dashboard', component: QuesBankTableComponent }
 ];
