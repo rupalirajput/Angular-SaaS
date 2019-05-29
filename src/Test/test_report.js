a592e31ba845c9a74500b72ca3deaf3f5cb6cb10
Chris Choi <choic2@seattleu.edu>
2019-05-29 14:36:20 -0700

    added report mocha test

---
+++ src/Test/test_report.js
@@ -0,0 +1,48 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var async = require('async');
+
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+var http = require('http');
+chai.use(chaiHttp);
+
+describe('Check Report Object', function() {
+
+    var requestResult;
+    var response;
+
+    before(function (done){
+        chai.request("http://localhost:1234")
+            .get("/report/1/reports/1")
+            .end(function (err,res){
+                requestResult = res.body;
+                response = res;
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('Should return one JSON object', function(){
+        expect(response).to.have.status(200);
+        expect(response.body).to.have.length(1);
+        expect(response).to.have.headers;
+        expect(response).to.be.json;
+    })
+    it('The Oject has known properties', function(){
+        expect(requestResult[0]).to.include.keys('reportid');
+        expect(requestResult[0]).to.have.property('userid');
+        expect(requestResult[0]).to.have.property('quesBankID');
+        expect(requestResult[0]).to.have.property('score');
+        expect(requestResult[0]).to.have.property('strengths');
+        expect(requestResult[0]).to.have.property('weaknesses');
+        expect(requestResult[0]).to.have.property('categories');
+        expect(requestResult[0]).to.have.property('scores');
+        expect(requestResult[0]).to.have.property('title');
+    });
+    
+
+});
