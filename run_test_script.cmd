6d99c5ed644e930ee4300e0fe50472e4246d7df7
Jake Ladera <laderah@seattleu.edu>
2019-05-29 19:25:15 -0700

    Added a test for a single question object, which is being used in taketest

--- run_test_script.cmd
+++ run_test_script.cmd
@@ -1,1 +1,1 @@
-mocha --reporter spec src/Test/test_account.js src/Test/test_question.js src/Test/test_report.js
+mocha --reporter spec src/Test/test_account.js src/Test/test_question.js src/Test/test_report.js src/Test/test_taketest.js
